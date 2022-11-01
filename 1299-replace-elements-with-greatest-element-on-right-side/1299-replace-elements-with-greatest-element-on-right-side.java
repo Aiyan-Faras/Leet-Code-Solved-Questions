e1fbc2484ca6d87424e6ed5fadfb6c046cee1937
Aiyan Faras <farasaiyan@gmail.com>
2022-11-01 13:53:07 +0530

    Time: 8 ms (43.63%), Space: 55.1 MB (6.74%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -3,7 +3,7 @@ class Solution {
         int max = arr[arr.length-1];
         arr[arr.length-1] = -1;
         for(int i=arr.length-2; i>=0; i--){
-            int temp =arr[i];
+            int temp = arr[i];
             arr[i] = max;
             max = Math.max(temp,max);
         }
